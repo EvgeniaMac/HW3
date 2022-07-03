ddc37720da58ed34b3b8da8335ff4034a1d20729
«Evgenia <evgenia.kopitext@gmail.com>
2022-07-03 11:49:13 +1100

    Tasc021

---
+++ Tesc021/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход координаты
+// двух точек и находит расстояние между ними в 3D пространстве.
+// AB = √(xb - xa)2 + (yb - ya)2 + (zb - za)2
+
+int x1 = Convert.ToInt32(Console.ReadLine());
+int y1 = Convert.ToInt32(Console.ReadLine());
+int z1 = Convert.ToInt32(Console.ReadLine());
+int x2 = Convert.ToInt32(Console.ReadLine());
+int y2 = Convert.ToInt32(Console.ReadLine());
+int z2 = Convert.ToInt32(Console.ReadLine());
+
+double d = Math.Sqrt(Math.Pow(x1-x2,2) + Math.Pow(y1-y2,2) + Math.Pow(z1-z2,2));
+
+Console.WriteLine(d*100);
